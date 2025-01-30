7e7b3d0496270684a14823b79abfb0d22da9552c
Lelandrsc <lrcanda@cpp.edu>
2025-01-30 15:17:09 -0800

    Create Hello.cpp

---
+++ Lab1/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * {Short Decription ofthe lab}
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout << "Hello World!" << end1;
+    printf("Hello World again!\n");
+    return 0;
+}
